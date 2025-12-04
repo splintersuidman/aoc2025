32ad428d4d0452fa0e6c45763d8e9d7f445df8c9
Splinter Suidman <splinter.suidman@channable.com>
2025-12-04 16:04:12 +0100

    Update Flake

--- flake.nix
+++ flake.nix
@@ -1,44 +1,30 @@
 {
   description = "Flake for Advent of Code 2025";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05"; };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11"; };
 
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-      compiler = "ghc984";
     in {
       packages = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
-          haskellPackages = pkgs.haskell.packages.${compiler};
+          haskellPackages = pkgs.haskellPackages;
         in rec {
           default = aoc2025;
           aoc2025 = haskellPackages.callPackage ./aoc2025.nix { };
+
+          shell = self.packages.${system}.default.env.overrideAttrs (oldAttrs: {
+            buildInputs = (oldAttrs.buildInputs or [ ]) ++ [
+              haskellPackages.cabal2nix
+              haskellPackages.haskell-language-server
+            ];
+          });
         });
 
-      devShell = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-          haskellPackages = pkgs.haskell.packages.${compiler};
-        in self.packages.${system}.default.env.overrideAttrs (oldAttrs: {
-          buildInputs = (oldAttrs.buildInputs or [ ]) ++ [
-            haskellPackages.cabal2nix
-            haskellPackages.haskell-language-server
-          ];
-        }));
-      # devShell = forAllSystems (system:
-      #   let
-      #     pkgs = nixpkgsFor.${system};
-      #     haskellPackages = pkgs.haskell.packages.${compiler};
-      #   in pkgs.mkShell {
-      #     buildInputs = [
-      #       self.packages.${system}.default.env
-      #       haskellPackages.cabal2nix
-      #       haskellPackages.haskell-language-server
-      #     ];
-      #   });
+      devShell = forAllSystems (system: self.packages.${system}.shell);
     };
 }
